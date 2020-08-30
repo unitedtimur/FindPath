a25c3113c0244063642d3adf5c30bef8fde05a84
Timur Likhomanov <lihomanov.t@gmail.com>
2020-08-30 14:06:24 +0700

    optimization

--- src/movescene.cpp
+++ src/movescene.cpp
@@ -96,12 +96,36 @@ void MoveScene::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
     //BFSLogic bfcLogic(this->getCells(), this->w, this->h);
 }
 
+struct Point
+{
+public:
+    explicit Point()
+    {
+        x = 0;
+        y = 0;
+        code = 0;
+    }
+    explicit Point(int y, int x, int code)
+    {
+        this->x = x;
+        this->y = y;
+        this->code = code;
+    }
+
+    int x;
+    int y;
+    int code;
+};
+
 void MoveScene::bfs(qint32 w, qint32 h)
 {
     const auto parse = this->getCells();
 
     queue<GraphicsCell*> cells;
-    vector<vector<int>> matrixPath(h, vector<int>(w, 0));
+    vector<vector<Point>> matrixPath(h, vector<Point>(w));
+
+    qint32 cellY;
+    qint32 cellX;
 
     for (const auto& row : parse)
     {
@@ -113,94 +137,60 @@ void MoveScene::bfs(qint32 w, qint32 h)
             }
             else if (column->getStatus() == GraphicsCell::Status::BarrierCell)
             {
-                matrixPath[column->getY()][column->getX()] = -1;
+                cellY = column->getY();
+                cellX = column->getX();
+
+                matrixPath[cellY][cellX] = Point(cellY, cellX, -1);
             }
         }
     }
 
-    for (int i = 0; i < h; ++i)
-    {
-        for (int j = 0; j < w; ++j)
-        {
-            std::cout << matrixPath[i][j] << " ";
-        }
-
-        std::cout << std::endl;
-    }
-
-    std::cout << std::flush;
-
-    matrixPath[cells.front()->getY()][cells.front()->getX()] = 1;
-
+    matrixPath[cells.front()->getY()][cells.front()->getX()] = Point(cells.front()->getY(), cells.front()->getX(), 1);
 
     while (!cells.empty())
     {
         GraphicsCell* cell = cells.front();
 
-        if (cell->getY() != 0 && matrixPath[cell->getY() - 1][cell->getX()] == 0)
+        cellY = cell->getY();
+        cellX = cell->getX();
+
+        if (cellY != 0 && matrixPath[cellY - 1][cellX].code == 0)
         {
-            matrixPath[cell->getY() - 1][cell->getX()] = matrixPath[cell->getY()][cell->getX()] + 1;
-            cells.push(parse[cell->getY() - 1][cell->getX()]);
+            matrixPath[cellY - 1][cellX] = Point(cellY, cellX, matrixPath[cellY][cellX].code + 1);
+            cells.push(parse[cellY - 1][cellX]);
 
             if (cells.back()->getStatus() == GraphicsCell::Status::FinishCell)
                 break;
         }
 
-        for (int i = 0; i < h; ++i)
+        if (cellY != parse.size() - 1 && matrixPath[cellY + 1][cellX].code == 0)
         {
-            for (int j = 0; j < w; ++j)
-            {
-                std::cout << matrixPath[i][j] << " ";
-            }
-
-            std::cout << std::endl;
-        }
-
-        std::cout << std::endl << std::flush;
-
-        if (cell->getY() != parse.size() - 1 && matrixPath[cell->getY() + 1][cell->getX()] == 0)
-        {
-            matrixPath[cell->getY() + 1][cell->getX()] = matrixPath[cell->getY()][cell->getX()] + 1;
-            cells.push(parse[cell->getY() + 1][cell->getX()]);
+            matrixPath[cellY + 1][cellX] = Point(cellY, cellX, matrixPath[cellY][cellX].code + 1);
+            cells.push(parse[cellY + 1][cellX]);
 
             if (cells.back()->getStatus() == GraphicsCell::Status::FinishCell)
                 break;
         }
 
-        if (cell->getX() != 0 && matrixPath[cell->getY()][cell->getX() - 1] == 0)
+        if (cellX != 0 && matrixPath[cellY][cellX - 1].code == 0)
         {
-            matrixPath[cell->getY()][cell->getX() - 1] = matrixPath[cell->getY()][cell->getX()] + 1;
-            cells.push(parse[cell->getY()][cell->getX() - 1]);
+            matrixPath[cellY][cellX - 1] = Point(cellY, cellX, matrixPath[cellY][cellX].code + 1);
+            cells.push(parse[cellY][cellX - 1]);
 
             if (cells.back()->getStatus() == GraphicsCell::Status::FinishCell)
                 break;
         }
 
-        if (cell->getX() != parse[0].size() - 1 && matrixPath[cell->getY()][cell->getX() + 1] == 0)
+        if (cellX != parse[0].size() - 1 && matrixPath[cellY][cellX + 1].code == 0)
         {
-            matrixPath[cell->getY()][cell->getX() + 1] = matrixPath[cell->getY()][cell->getX()] + 1;
-            cells.push(parse[cell->getY()][cell->getX() + 1]);
+            matrixPath[cellY][cellX + 1] = Point(cellY, cellX, matrixPath[cellY][cellX].code + 1);
+            cells.push(parse[cellY][cellX + 1]);
 
             if (cells.back()->getStatus() == GraphicsCell::Status::FinishCell)
                 break;
         }
-
 
         cells.pop();
-
-
-        for (int i = 0; i < h; ++i)
-        {
-            for (int j = 0; j < w; ++j)
-            {
-                std::cout << matrixPath[i][j] << " ";
-            }
-
-            std::cout << std::endl;
-        }
-
-        std::cout << std::endl << std::flush;
-
     }
 
     if (cells.empty())
@@ -209,46 +199,22 @@ void MoveScene::bfs(qint32 w, qint32 h)
         return;
     }
 
-    for (int i = 0; i < h; ++i)
-    {
-        for (int j = 0; j < w; ++j)
-        {
-            std::cout << matrixPath[i][j] << " ";
-        }
+    cellX = cells.back()->getX();
+    cellY = cells.back()->getY();
 
-        std::cout << std::endl;
+    qint32 currentNumber = matrixPath[cellY][cellX].code;
+
+    for (qint32 i = 0; i < currentNumber; ++i)
+    {
+        parse[cellY][cellX]->setStatus(GraphicsCell::Status::PathCell);
+
+        qint32 tempX = cellX;
+        qint32 tempY = cellY;
+
+        cellY = matrixPath[tempY][tempX].y;
+        cellX = matrixPath[tempY][tempX].x;
     }
 
-    std::cout << std::flush;
-
-    qint32 x = cells.back()->getX();
-    qint32 y = cells.back()->getY();
-
-    qint32 currentNumber = matrixPath[y][x];
-
-    while (currentNumber != 0)
-    {
-        parse[y][x]->setStatus(GraphicsCell::Status::PathCell);
-
-        if (y != 0 && matrixPath[y - 1][x] == currentNumber - 1)
-        {
-            --y;
-        }
-        else if (y != parse.size() - 1 && matrixPath[y + 1][x] == currentNumber - 1)
-        {
-            ++y;
-        }
-        else if (x != 0 && matrixPath[y][x - 1] == currentNumber - 1)
-        {
-            --x;
-        }
-        else if (x != parse[0].size() - 1 && matrixPath[y][x + 1] == currentNumber - 1)
-        {
-            ++x;
-        }
-
-        --currentNumber;
-    }
 }
 
 void MoveScene::generatedField(const qint32& w, const qint32& h)
